be7b6027e358f11802098492a679ea9d14a87686
butzist <>
2002-10-23 16:21:27 +0000

    a .cpl for configuring the server

---
+++ win32-server/CPL/StdAfx.h
@@ -0,0 +1,27 @@
+// stdafx.h : Include-Datei für Standard-System-Include-Dateien,
+//  oder projektspezifische Include-Dateien, die häufig benutzt, aber
+//      in unregelmäßigen Abständen geändert werden.
+//
+
+#if !defined(AFX_STDAFX_H__DBBBD03F_C050_44A0_8368_696184A2D7D5__INCLUDED_)
+#define AFX_STDAFX_H__DBBBD03F_C050_44A0_8368_696184A2D7D5__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+
+// Fügen Sie hier Ihre Header-Dateien ein
+#define WIN32_LEAN_AND_MEAN		// Selten benutzte Teile der Windows-Header nicht einbinden
+
+#include <windows.h>
+#include <winreg.h>
+#include <cpl.h>
+#include <commctrl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ fügt zusätzliche Deklarationen unmittelbar vor der vorherigen Zeile ein.
+
+#endif // !defined(AFX_STDAFX_H__DBBBD03F_C050_44A0_8368_696184A2D7D5__INCLUDED_)
---
+++ win32-server/CPL/resource.h
@@ -0,0 +1,32 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by CPL.rc
+//
+#define IDS_NAME                        1
+#define IDS_DESC                        2
+#define IDD_DIALOG1                     1000
+#define IDD_MAINDIALOG                  1000
+#define IDC_NAME                        1002
+#define IDC_LOGFILE                     1002
+#define IDC_PORT                        1003
+#define IDB_LOGO                        1004
+#define IDI_ICON                        1005
+#define IDC_IPADDRESS                   1006
+#define IDC_USERS                       1007
+#define IDC_HOSTS_ALLOW                 1008
+#define IDC_HOSTS_DENY                  1009
+#define IDC_BUTTON1                     1010
+#define IDC_EDIT1                       1011
+#define IDC_EDIT2                       1012
+#define IDC_BUTTON2                     1013
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        1006
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_SYMED_VALUE           1000
+#endif
+#endif
